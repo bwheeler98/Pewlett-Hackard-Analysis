afd067975024d510094cb6b7d2ba53a09b5c5fbb
bwheeler98 <100613516+bwheeler98@users.noreply.github.com>
2022-05-05 21:36:55 -0400

    deliverable 2 completed.

--- Pewlett-Hacker-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hacker-Analysis/Queries/Employee_Database_challenge.sql
@@ -28,3 +28,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+--Deliverable 2
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_employees AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tt
+ON (e.emp_no = tt.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
